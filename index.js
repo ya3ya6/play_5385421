44d13e37b127e848a1e8c7f66fb3b434c7ad1749
ya3ya6 <ya3ya63@gmail.com>
2024-04-10 06:43:08 +0330

    dasdsaasfs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
    res.send('hey');
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT || 3000, () => {
+const server = app.listen(PORT || 3000, () => {
   console.log('listening on port ' + PORT);
 });
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
